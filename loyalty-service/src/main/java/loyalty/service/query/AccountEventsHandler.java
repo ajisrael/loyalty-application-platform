b861a47d20221279cf7908627513ce19171d0cae
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-09-11 12:51:27 -0400

    f   - added update account feature on query api

--- loyalty-service/src/main/java/loyalty/service/query/AccountEventsHandler.java
+++ loyalty-service/src/main/java/loyalty/service/query/AccountEventsHandler.java
@@ -3,6 +3,8 @@ package loyalty.service.query;
 import loyalty.service.core.data.AccountEntity;
 import loyalty.service.core.data.AccountRepository;
 import loyalty.service.core.events.AccountCreatedEvent;
+import loyalty.service.core.events.AccountUpdatedEvent;
+import loyalty.service.core.exceptions.AccountNotFoundException;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.messaging.interceptors.ExceptionHandler;
@@ -10,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @ProcessingGroup("account-group")
@@ -38,5 +42,18 @@ public class AccountEventsHandler {
         AccountEntity accountEntity = new AccountEntity();
         BeanUtils.copyProperties(event, accountEntity);
         accountRepository.save(accountEntity);
+    }
+
+    @EventHandler
+    public void on(AccountUpdatedEvent event) {
+        Optional<AccountEntity> accountEntityOptional = accountRepository.findByAccountId(event.getAccountId());
+
+        if (accountEntityOptional.isPresent()) {
+            AccountEntity accountEntity = accountEntityOptional.get();
+            BeanUtils.copyProperties(event, accountEntity);
+            accountRepository.save(accountEntity);
+        } else {
+            throw new AccountNotFoundException(event.getAccountId());
+        }
     }
 }
