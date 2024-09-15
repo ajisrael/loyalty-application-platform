8b64d6a5bf7baf3d47174b16543f15b3e5ca04b2
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-09-15 00:30:55 -0400

    f   - completed docker integration

--- deploy_loyalty_service_local.sh
+++ deploy_loyalty_service_local.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Check for --build flag
@@ -6,6 +5,15 @@ BUILD=false
 for arg in "$@"; do
   if [ "$arg" == "--build" ]; then
     BUILD=true
+    break
+  fi
+done
+
+# Check for --build-all flag
+BUILD_ALL=false
+for arg in "$@"; do
+  if [ "$arg" == "--build-all" ]; then
+    BUILD_ALL=true
     break
   fi
 done
@@ -27,45 +35,48 @@ else
     docker compose down
 fi
 
-if [ "$BUILD" = true ]; then
-    echo "Building discovery-server"
-    cd ./discovery-server
-
-    mvn clean package -Dmaven.test.skip=true
-
-    # Check if JAR file exists
-    if [ ! -f target/DiscoveryServer-0.0.1-SNAPSHOT.jar ]; then
-	echo "Error: JAR file not found for discovery-server"
-	exit 1
-    fi
-
-    cd ..
-
-    echo "Building api-gateway"
-    cd ./api-gateway
-
-    mvn clean package -Dmaven.test.skip=true
-
-    # Check if JAR file exists
-    if [ ! -f target/ApiGateway-0.0.1-SNAPSHOT.jar ]; then
-	echo "Error: JAR file not found for api-gateway"
-	exit 1
-    fi
-
-    cd ..
-
+if [ "$BUILD" = true ] || [ "$BUILD_ALL" = true ]; then
     echo "Building loyalty-service"
-    cd ./loyalty-service
+    cd ./loyalty-service || exit 1
 
     mvn clean package -Dmaven.test.skip=true
 
     # Check if JAR file exists
     if [ ! -f target/loyalty-service-0.0.1-SNAPSHOT.jar ]; then
-	echo "Error: JAR file not found for loyalty-service"
-	exit 1
+      echo "Error: JAR file not found for loyalty-service"
+      exit 1
     fi
 
     cd ..
+fi
+
+if [ "$BUILD_ALL" = true ]; then
+    echo "Building discovery-server"
+    cd ./discovery-server || exit 1
+
+    mvn clean package -Dmaven.test.skip=true
+
+    # Check if JAR file exists
+    if [ ! -f target/DiscoveryServer-0.0.1-SNAPSHOT.jar ]; then
+      echo "Error: JAR file not found for discovery-server"
+      exit 1
+    fi
+
+    cd ..
+
+    echo "Building api-gateway"
+    cd ./api-gateway || exit 1
+
+    mvn clean package -Dmaven.test.skip=true
+
+    # Check if JAR file exists
+    if [ ! -f target/ApiGateway-0.0.1-SNAPSHOT.jar ]; then
+      echo "Error: JAR file not found for api-gateway"
+      exit 1
+    fi
+
+    cd ..
+
 else
     echo "Skipping build as --build flag was not provided."
 fi
