0a4b3273b534079d93b4056a55f3bf11087ac62b
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-09-24 22:45:34 -0400

    b   - fixed account update to not allow another accounts email

--- loyalty-command-api/src/main/java/loyalty/service/command/interceptors/UpdateAccountCommandInterceptor.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/interceptors/UpdateAccountCommandInterceptor.java
@@ -3,6 +3,8 @@ package loyalty.service.command.interceptors;
 import loyalty.service.command.commands.UpdateAccountCommand;
 import loyalty.service.command.data.entities.AccountLookupEntity;
 import loyalty.service.command.data.repositories.AccountLookupRepository;
+import loyalty.service.core.exceptions.EmailExistsForAccountException;
+import loyalty.service.core.utils.MarkerGenerator;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.messaging.MessageDispatchInterceptor;
@@ -24,12 +26,6 @@ public class UpdateAccountCommandInterceptor implements MessageDispatchIntercept
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UpdateAccountCommandInterceptor.class);
 
-    @Autowired
-    private QueryGateway queryGateway;
-
-    @Autowired
-    private CommandGateway commandGateway;
-
     private final AccountLookupRepository accountLookupRepository;
 
     public UpdateAccountCommandInterceptor(AccountLookupRepository accountLookupRepository) {
@@ -42,18 +38,27 @@ public class UpdateAccountCommandInterceptor implements MessageDispatchIntercept
         return (index, command) -> {
 
             if (UpdateAccountCommand.class.equals(command.getPayloadType())) {
-                LOGGER.info(String.format(INTERCEPTED_COMMAND, command.getPayloadType()));
+                LOGGER.info(
+                        MarkerGenerator.generateMarker(command),
+                        String.format(INTERCEPTED_COMMAND, command.getClass().getSimpleName())
+                );
 
                 UpdateAccountCommand updateAccountCommand = (UpdateAccountCommand) command.getPayload();
 
                 updateAccountCommand.validate();
 
                 String accountId = updateAccountCommand.getAccountId();
-
                 AccountLookupEntity accountLookupEntity = accountLookupRepository.findByAccountId(accountId);
 
                 throwExceptionIfEntityDoesNotExist(accountLookupEntity,
                         String.format(ACCOUNT_WITH_ID_DOES_NOT_EXIST, accountId));
+
+                String email = updateAccountCommand.getEmail();
+                AccountLookupEntity emailAccountLookupEntity = accountLookupRepository.findByEmail(email);
+
+                if (emailAccountLookupEntity != null && !emailAccountLookupEntity.getAccountId().equals(accountId)) {
+                    throw new EmailExistsForAccountException(email);
+                }
             }
 
             return command;
--- loyalty-service-core/src/main/java/loyalty/service/core/exceptions/EmailExistsForAccountException.java
+++ loyalty-service-core/src/main/java/loyalty/service/core/exceptions/EmailExistsForAccountException.java
@@ -1,9 +1,9 @@
 package loyalty.service.core.exceptions;
 
-import static loyalty.service.core.constants.ExceptionMessages.EMAIL_ALREADY_EXISTS_FOR_AN_ACCOUNT;
+import static loyalty.service.core.constants.ExceptionMessages.EMAIL_ALREADY_EXISTS_FOR_ANOTHER_ACCOUNT;
 
 public class EmailExistsForAccountException extends RuntimeException {
     public EmailExistsForAccountException(String email) {
-        super(String.format(EMAIL_ALREADY_EXISTS_FOR_AN_ACCOUNT, email));
+        super(String.format(EMAIL_ALREADY_EXISTS_FOR_ANOTHER_ACCOUNT, email));
     }
 }
