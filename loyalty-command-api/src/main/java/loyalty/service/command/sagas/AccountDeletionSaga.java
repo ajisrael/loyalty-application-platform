777fd53d060b0870ba04e9abe0c343f31dd734a8
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-09-26 15:25:33 -0400

    f   - added logging to saga

--- loyalty-command-api/src/main/java/loyalty/service/command/sagas/AccountDeletionSaga.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/sagas/AccountDeletionSaga.java
@@ -7,15 +7,22 @@ import loyalty.service.command.data.repositories.LoyaltyBankLookupRepository;
 import loyalty.service.core.events.AccountDeletedEvent;
 import loyalty.service.core.events.LoyaltyBankDeletedEvent;
 import loyalty.service.core.events.AllPointsExpiredEvent;
+import loyalty.service.core.utils.MarkerGenerator;
+import net.logstash.logback.marker.Markers;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.SagaLifecycle;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
 import java.util.List;
+
+import static loyalty.service.core.constants.DomainConstants.REQUEST_ID;
 
 @Saga
 public class AccountDeletionSaga implements Serializable {
@@ -25,42 +32,73 @@ public class AccountDeletionSaga implements Serializable {
     @Autowired
     private transient LoyaltyBankLookupRepository loyaltyBankLookupRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountDeletionSaga.class);
+
     @SagaEventHandler(associationProperty = "accountId")
     @StartSaga
     public void handle(AccountDeletedEvent event) {
         String accountId = event.getAccountId();
         SagaLifecycle.associateWith("accountId", accountId);
 
+        Marker marker = Markers.append(REQUEST_ID, event.getRequestId());
+        LOGGER.info(marker, "{} received, checking for loyaltyBankEntities", event.getClass().getSimpleName());
+
         List<LoyaltyBankLookupEntity> loyaltyBankLookupEntities = loyaltyBankLookupRepository.findByAccountId(accountId);
 
         if (loyaltyBankLookupEntities.isEmpty()) {
+            LOGGER.info(marker, "No loyaltyBankEntities found, ending saga");
             SagaLifecycle.end();
             return;
         }
 
         loyaltyBankLookupEntities.forEach(
-                loyaltyBankLookupEntity -> commandGateway.send(
-                        ExpireAllPointsCommand.builder()
-                                .loyaltyBankId(loyaltyBankLookupEntity.getLoyaltyBankId())
-                                .build()
-                )
+                loyaltyBankLookupEntity -> {
+                    ExpireAllPointsCommand command = ExpireAllPointsCommand.builder()
+                            .requestId(event.getRequestId())
+                            .loyaltyBankId(loyaltyBankLookupEntity.getLoyaltyBankId())
+                            .build();
+
+                    LOGGER.info(
+                            MarkerGenerator.generateMarker(command),
+                            "Sending {} command",
+                            command.getClass().getSimpleName()
+                    );
+
+                    commandGateway.send(command);
+                }
         );
     }
 
     @SagaEventHandler(associationProperty = "accountId")
     public void handle(AllPointsExpiredEvent event) {
-        commandGateway.send(DeleteLoyaltyBankCommand.builder()
+        DeleteLoyaltyBankCommand command = DeleteLoyaltyBankCommand.builder()
+                .requestId(event.getRequestId())
                 .loyaltyBankId(event.getLoyaltyBankId())
-                .build()
-        );
+                .build();
+
+        Marker marker = MarkerGenerator.generateMarker(command);
+        marker.add(Markers.append(REQUEST_ID, command.getRequestId()));
+        LOGGER.info(marker, "{} received, issuing {}", event.getClass().getSimpleName(), command.getClass().getSimpleName());
+
+        commandGateway.send(command);
     }
 
     @SagaEventHandler(associationProperty = "accountId")
     public void handle(LoyaltyBankDeletedEvent event) {
+        Marker marker = Markers.append(REQUEST_ID, event.getRequestId());
+        LOGGER.info(marker, "{} received, checking for loyaltyBankEntities", event.getClass().getSimpleName());
+
         List<LoyaltyBankLookupEntity> loyaltyBankLookupEntities = loyaltyBankLookupRepository.findByAccountId(event.getAccountId());
 
         if (loyaltyBankLookupEntities.isEmpty()) {
+            LOGGER.info(marker, "No loyaltyBankEntities found, ending saga");
             SagaLifecycle.end();
+            return;
         }
+
+        LOGGER.info(
+                marker, "{} loyaltyBankEntities remaining, continuing saga lifecycle",
+                loyaltyBankLookupEntities.size()
+        );
     }
 }
