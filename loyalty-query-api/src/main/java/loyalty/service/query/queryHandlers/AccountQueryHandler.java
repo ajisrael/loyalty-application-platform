5d25156fa2180ead72713fc94ba6a3df866c9597
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-02 10:33:14 -0400

    f   - added logging to account query handler

--- loyalty-query-api/src/main/java/loyalty/service/query/queryHandlers/AccountQueryHandler.java
+++ loyalty-query-api/src/main/java/loyalty/service/query/queryHandlers/AccountQueryHandler.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import loyalty.service.core.data.entities.AccountEntity;
 import loyalty.service.core.data.repositories.AccountRepository;
 import loyalty.service.core.exceptions.AccountNotFoundException;
+import loyalty.service.core.utils.MarkerGenerator;
 import loyalty.service.query.queries.FindAccountQuery;
 import loyalty.service.query.queries.FindAllAccountsQuery;
 import loyalty.service.query.querymodels.AccountQueryModel;
 import org.axonframework.queryhandling.QueryHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
@@ -15,16 +18,21 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class AccountQueryHandler {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(AccountQueryHandler.class);
     private final AccountRepository accountRepository;
 
     @QueryHandler
     public Page<AccountQueryModel> findAllAccounts(FindAllAccountsQuery query) {
+        LOGGER.info(MarkerGenerator.generateMarker(query), "Processing {}", query.getClass().getSimpleName());
+
         return accountRepository.findAll(query.getPageable())
                 .map(this::convertAccountEntityToAccountQueryModel);
     }
 
     @QueryHandler
     public AccountQueryModel findAccount(FindAccountQuery query) {
+        LOGGER.info(MarkerGenerator.generateMarker(query), "Processing {}", query.getClass().getSimpleName());
+
         AccountEntity accountEntity = accountRepository.findById(query.getAccountId()).orElseThrow(
                 () -> new AccountNotFoundException(query.getAccountId()));
         return convertAccountEntityToAccountQueryModel(accountEntity);
