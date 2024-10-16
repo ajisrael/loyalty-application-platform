a21f57be1b0385f3b9597bc1bbf79e8342e7fdec
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-16 04:41:10 -0400

    t   - added test for cannot update a non existing account

--- loyalty-command-api/src/test/java/loyalty/service/command/aggregates/AccountAggregateTest.java
+++ loyalty-command-api/src/test/java/loyalty/service/command/aggregates/AccountAggregateTest.java
@@ -5,6 +5,7 @@ import loyalty.service.command.commands.UpdateAccountCommand;
 import loyalty.service.core.events.AccountCreatedEvent;
 import loyalty.service.core.events.AccountUpdatedEvent;
 import org.axonframework.eventsourcing.eventstore.EventStoreException;
+import org.axonframework.modelling.command.AggregateNotFoundException;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,6 +26,8 @@ class AccountAggregateTest {
     private static final String TEST_FIRST_NAME = "John";
     private static final String TEST_LAST_NAME = "Doe";
     private static final String TEST_EMAIL = "john.doe@test.com";
+    private static final String TEST_NEW_FIRST_NAME = "Johnathan";
+
     private static final CreateAccountCommand createAccountCommand = CreateAccountCommand.builder()
             .requestId(TEST_REQUEST_ID)
             .accountId(TEST_ACCOUNT_ID)
@@ -39,6 +42,22 @@ class AccountAggregateTest {
             .firstName(createAccountCommand.getFirstName())
             .lastName(createAccountCommand.getLastName())
             .email(createAccountCommand.getEmail())
+            .build();
+
+    private static final UpdateAccountCommand updateAccountCommand = UpdateAccountCommand.builder()
+            .requestId(TEST_REQUEST_ID)
+            .accountId(TEST_ACCOUNT_ID)
+            .firstName(TEST_NEW_FIRST_NAME)
+            .lastName(TEST_LAST_NAME)
+            .email(TEST_EMAIL)
+            .build();
+
+    private static final AccountUpdatedEvent accountUpdatedEvent = AccountUpdatedEvent.builder()
+            .requestId(updateAccountCommand.getRequestId())
+            .accountId(updateAccountCommand.getAccountId())
+            .firstName(updateAccountCommand.getFirstName())
+            .lastName(updateAccountCommand.getLastName())
+            .email(updateAccountCommand.getEmail())
             .build();
 
     @BeforeEach
@@ -66,26 +85,17 @@ class AccountAggregateTest {
     @Test
     @DisplayName("UpdateAccountCommand results in AccountUpdatedEvent")
     void testUpdateAccount_whenUpdateAccountCommandHandled_ShouldIssueAccountUpdatedEvent() {
-        // Arrange
-        UpdateAccountCommand updateAccountCommand = UpdateAccountCommand.builder()
-                .requestId(TEST_REQUEST_ID)
-                .accountId(TEST_ACCOUNT_ID)
-                .firstName("Johnathan")
-                .lastName(TEST_LAST_NAME)
-                .email(TEST_EMAIL)
-                .build();
-
-        AccountUpdatedEvent accountUpdatedEvent = AccountUpdatedEvent.builder()
-                .requestId(updateAccountCommand.getRequestId())
-                .accountId(updateAccountCommand.getAccountId())
-                .firstName(updateAccountCommand.getFirstName())
-                .lastName(updateAccountCommand.getLastName())
-                .email(updateAccountCommand.getEmail())
-                .build();
-
         // Act & Assert
         fixture.given(accountCreatedEvent)
                 .when(updateAccountCommand)
                 .expectEvents(accountUpdatedEvent);
+    }
+
+    @Test
+    @DisplayName("Cannot update an acount that hasn't been created")
+    void testUpdateAccount_whenUpdateAccountCommandHandledWithNoPriorActivity_shouldThrowException() {
+        fixture.givenNoPriorActivity()
+                .when(updateAccountCommand)
+                .expectException(AggregateNotFoundException.class);
     }
 }
