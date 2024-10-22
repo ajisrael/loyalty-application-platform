e030f852cdeaed3a8526a124bcdafbb4727b6c1d
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-22 11:40:10 -0400

    b   - added missing validation for targetTransactionId to expire points command

--- loyalty-command-api/src/main/java/loyalty/service/command/commands/transactions/CreateExpirePointsTransactionCommand.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/commands/transactions/CreateExpirePointsTransactionCommand.java
@@ -3,8 +3,17 @@ package loyalty.service.command.commands.transactions;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import static loyalty.service.core.constants.ExceptionMessages.TARGET_TRANSACTION_ID_CANNOT_BE_EMPTY;
+import static loyalty.service.core.utils.Helper.throwExceptionIfParameterIsNullOrBlank;
+
 @Getter
 @SuperBuilder
 public class CreateExpirePointsTransactionCommand extends AbstractTransactionCommand  {
     private String targetTransactionId;
+
+    @Override
+    public void validate() {
+        super.validate();
+        throwExceptionIfParameterIsNullOrBlank(this.getTargetTransactionId(), TARGET_TRANSACTION_ID_CANNOT_BE_EMPTY);
+    }
 }
