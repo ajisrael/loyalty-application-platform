dae99046b535c7950d637b4144b408d4d9bb3b42
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-10-22 14:24:12 -0400

    t   - fixed context test

--- loyalty-command-api/src/test/java/loyalty/service/LoyaltyServiceApplicationTests.java
+++ loyalty-command-api/src/test/java/loyalty/service/LoyaltyServiceApplicationTests.java
@@ -1,15 +1,15 @@
 package loyalty.service;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LoyaltyServiceApplicationTests {
 
-//	@Test
-//	@Disabled // TODO: identify why context doesn't load for test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
