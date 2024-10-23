514d1bcdbde7d41d5837c13f61ae7842d1eb6773
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-10-23 11:22:40 -0400

    f   - added validation for loyalty bank lookup entity

--- loyalty-command-api/src/main/java/loyalty/service/command/data/entities/LoyaltyBankLookupEntity.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/data/entities/LoyaltyBankLookupEntity.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import loyalty.service.core.validation.ProjectionId;
 
 @Data
 @NoArgsConstructor
@@ -16,10 +18,18 @@ import lombok.NoArgsConstructor;
 public class LoyaltyBankLookupEntity {
 
     @Id
+    @ProjectionId(message = "LoyaltyBankId should be valid")
+    @NotNull(message = "LoyaltyBankId cannot be null")
     @Column(name = "loyalty_bank_id", unique = true)
     private String loyaltyBankId;
+
+    @ProjectionId(message = "AccountId should be valid")
+    @NotNull(message = "AccountId cannot be null")
     @Column(name = "account_id")
     private String accountId;
+
+    @ProjectionId(message = "BusinessId should be valid")
+    @NotNull(message = "BusinessId cannot be null")
     @Column(name = "business_id")
     private String businessId;
 }
