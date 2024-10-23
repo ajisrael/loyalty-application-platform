751f709669a56fe85c0329af3e2f67a3e1bb66cd
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-10-23 00:53:05 -0400

    F   - added validation for account lookup entity to event handlers

--- loyalty-command-api/src/main/java/loyalty/service/command/projections/AccountLookupEventsHandler.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/projections/AccountLookupEventsHandler.java
@@ -15,6 +15,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.SmartValidator;
+import org.springframework.validation.annotation.Validated;
 
 import static loyalty.service.core.constants.DomainConstants.REQUEST_ID;
 import static loyalty.service.core.constants.ExceptionMessages.ACCOUNT_WITH_ID_DOES_NOT_EXIST;
@@ -23,10 +27,12 @@ import static loyalty.service.core.utils.Helper.throwExceptionIfEntityDoesNotExi
 
 @Component
 @AllArgsConstructor
+@Validated
 @ProcessingGroup("account-lookup-group")
 public class AccountLookupEventsHandler {
 
     private AccountLookupRepository accountLookupRepository;
+    private final SmartValidator validator;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountLookupEventsHandler.class);
 
@@ -42,12 +48,10 @@ public class AccountLookupEventsHandler {
 
     @EventHandler
     public void on(AccountCreatedEvent event) {
-        accountLookupRepository.save(
-                new AccountLookupEntity(
-                        event.getAccountId(),
-                        event.getEmail()
-                )
-        );
+        AccountLookupEntity accountLookupEntity = new AccountLookupEntity(event.getAccountId(), event.getEmail());
+
+        validateEntity(accountLookupEntity);
+        accountLookupRepository.save(accountLookupEntity);
 
         LOGGER.info(Markers.append(REQUEST_ID, event.getRequestId()), ACCOUNT_SAVED_IN_LOOKUP_DB, event.getAccountId());
     }
@@ -56,7 +60,9 @@ public class AccountLookupEventsHandler {
     public void on(AccountUpdatedEvent event) {
         AccountLookupEntity accountLookupEntity = accountLookupRepository.findByAccountId(event.getAccountId());
         throwExceptionIfEntityDoesNotExist(accountLookupEntity, String.format(ACCOUNT_WITH_ID_DOES_NOT_EXIST, event.getAccountId()));
+
         BeanUtils.copyProperties(event, accountLookupEntity);
+        validateEntity(accountLookupEntity);
         accountLookupRepository.save(accountLookupEntity);
 
         LOGGER.info(Markers.append(REQUEST_ID, event.getRequestId()), ACCOUNT_UPDATED_IN_LOOKUP_DB, event.getAccountId());
@@ -69,5 +75,14 @@ public class AccountLookupEventsHandler {
         accountLookupRepository.delete(accountLookupEntity);
 
         LOGGER.info(Markers.append(REQUEST_ID, event.getRequestId()), ACCOUNT_DELETED_FROM_LOOKUP_DB, event.getAccountId());
+    }
+
+    private void validateEntity(AccountLookupEntity entity) {
+        BindingResult bindingResult = new BeanPropertyBindingResult(entity, "accountLookupEntity");
+        validator.validate(entity, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException(bindingResult.getFieldError().getDefaultMessage());
+        }
     }
 }
