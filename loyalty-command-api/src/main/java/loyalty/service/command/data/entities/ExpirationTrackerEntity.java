5749f5be38ad07ca5e2451ff55fa2f0c015524eb
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-23 03:07:38 -0400

    f   - added additional validation for expiration tracker and transaction entities

--- loyalty-command-api/src/main/java/loyalty/service/command/data/entities/ExpirationTrackerEntity.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/data/entities/ExpirationTrackerEntity.java
@@ -1,8 +1,10 @@
 package loyalty.service.command.data.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import loyalty.service.core.validation.ProjectionId;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.List;
 public class ExpirationTrackerEntity {
 
     @Id
+    @ProjectionId(message = "LoyaltyBankId should be valid")
+    @NotNull(message = "LoyaltyBankId cannot be null")
     @Column(name = "loyalty_bank_id", unique = true)
     private String loyaltyBankId;
 
--- loyalty-command-api/src/main/java/loyalty/service/command/data/entities/TransactionEntity.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/data/entities/TransactionEntity.java
@@ -1,7 +1,9 @@
 package loyalty.service.command.data.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import loyalty.service.core.validation.ProjectionId;
 
 import java.time.Instant;
 
@@ -11,16 +13,22 @@ import java.time.Instant;
 @Entity
 @Table(name = "transaction")
 public class TransactionEntity {
+
     @Id
+    @ProjectionId(message = "TransactionId should be Valid")
+    @NotNull(message = "TransactionId cannot be null")
     @Column(name = "transaction_id")
     private String transactionId;
 
     @Column(name = "points")
     private int points;
 
+    @NotNull(message = "Timestamp cannot be null")
     @Column(name = "timestamp")
     private Instant timestamp;
 
+    @ProjectionId(message = "LoyaltyBankId should be valid")
+    @NotNull(message = "LoyaltyBankId cannot be null")
     @Column(name = "expiration_tracker_id")
     private String loyaltyBankId;
 
