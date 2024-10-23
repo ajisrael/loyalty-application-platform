e9c11ef71cb476fb56919fdbbc18d3bbcb34362e
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-10-23 02:00:12 -0400

    f   - added additional validation for business lookup entity

--- loyalty-command-api/src/main/java/loyalty/service/command/data/entities/BusinessLookupEntity.java
+++ loyalty-command-api/src/main/java/loyalty/service/command/data/entities/BusinessLookupEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +19,10 @@ public class BusinessLookupEntity {
 
     @Id
     @Column(name = "business_id", unique = true)
+    @NotNull(message = "BusinessId cannot be null")
+    @Pattern(
+            regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+            message = "BusinessId must be a valid UUID"
+    )
     private String businessId;
 }
