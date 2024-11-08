badc2a0fdad767c62520e35ba6da8efa7f6b6af5
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-11-08 13:33:19 -0500

    t   - completed business lookup events handler test

--- loyalty-command-api/src/test/java/loyalty/service/command/projections/BusinessLookupEventsHandlerTest.java
+++ loyalty-command-api/src/test/java/loyalty/service/command/projections/BusinessLookupEventsHandlerTest.java
@@ -4,11 +4,9 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import loyalty.service.command.data.entities.AccountLookupEntity;
 import loyalty.service.command.data.entities.BusinessLookupEntity;
 import loyalty.service.command.data.repositories.BusinessLookupRepository;
-import loyalty.service.core.events.AccountCreatedEvent;
-import loyalty.service.core.events.BusinessEnrolledEvent;
+import loyalty.service.core.events.*;
 import loyalty.service.core.exceptions.IllegalProjectionStateException;
 import net.logstash.logback.marker.Markers;
 import org.junit.jupiter.api.AfterEach;
@@ -28,8 +26,9 @@ import java.util.List;
 import java.util.UUID;
 
 import static loyalty.service.core.constants.DomainConstants.*;
-import static loyalty.service.core.constants.LogMessages.ACCOUNT_SAVED_IN_LOOKUP_DB;
-import static loyalty.service.core.constants.LogMessages.BUSINESS_SAVED_IN_LOOKUP_DB;
+import static loyalty.service.core.constants.ExceptionMessages.ACCOUNT_WITH_ID_DOES_NOT_EXIST;
+import static loyalty.service.core.constants.ExceptionMessages.BUSINESS_WITH_ID_DOES_NOT_EXIST;
+import static loyalty.service.core.constants.LogMessages.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -159,6 +158,152 @@ class BusinessLookupEventsHandlerTest {
         // Assert
         assertEquals(exceptionMessage, exception.getLocalizedMessage());
         verify(businessLookupRepository, times(0)).save(any(BusinessLookupEntity.class));
+
+        List<ILoggingEvent> loggedEvents = listAppender.list;
+        assertEquals(0, loggedEvents.size());
+    }
+
+    @Test
+    @DisplayName("Can update existing BusinessLookupEntity on valid BusinessUpdatedEvent")
+    void testOn_whenValidBusinessUpdatedEventReceived_shouldUpdateBusinessLookupEntity() {
+        // Arrange
+        BusinessUpdatedEvent event = BusinessUpdatedEvent.builder()
+                .requestId(TEST_REQUEST_ID)
+                .businessId(TEST_BUSINESS_ID)
+                .businessName(TEST_BUSINESS_NAME)
+                .build();
+
+        when(businessLookupRepository.findByBusinessId(TEST_BUSINESS_ID)).thenReturn(new BusinessLookupEntity(TEST_BUSINESS_ID));
+
+        // Act
+        businessLookupEventsHandler.on(event);
+
+        // Assert
+        verify(businessLookupRepository, times(1)).findByBusinessId(TEST_BUSINESS_ID);
+        verify(businessLookupRepository, times(1)).save(any(BusinessLookupEntity.class));
+
+        List<ILoggingEvent> loggedEvents = listAppender.list;
+        assertEquals(1, loggedEvents.size());
+
+        loggingEvent = loggedEvents.get(0);
+        assertEquals(Level.INFO, loggingEvent.getLevel());
+        String expectedLogMessage = MessageFormatter.format(BUSINESS_UPDATED_IN_LOOKUP_DB, TEST_BUSINESS_ID).getMessage();
+        assertEquals(expectedLogMessage, loggingEvent.getFormattedMessage());
+
+        assertTrue(loggingEvent.getMarkerList().get(0).contains(Markers.append(REQUEST_ID, event.getRequestId())));
+        assertTrue(loggingEvent.getMarkerList().get(0).contains(Markers.append(BUSINESS_ID, event.getBusinessId())));
+    }
+
+    @Test
+    @DisplayName("Cannot update BusinessLookupEntity for business that doesn't exist")
+    void testOn_whenBusinessUpdatedEventReceivedForNonExistingBusiness_shouldThrowException() {
+        // Arrange
+        BusinessUpdatedEvent event = BusinessUpdatedEvent.builder()
+                .requestId(TEST_REQUEST_ID)
+                .businessId(TEST_BUSINESS_ID)
+                .businessName(TEST_BUSINESS_NAME)
+                .build();
+
+        when(businessLookupRepository.findByBusinessId(TEST_BUSINESS_ID)).thenReturn(null);
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            businessLookupEventsHandler.on(event);
+        });
+
+        // Assert
+        assertEquals(String.format(BUSINESS_WITH_ID_DOES_NOT_EXIST, event.getBusinessId()), exception.getLocalizedMessage());
+        verify(businessLookupRepository, times(1)).findByBusinessId(TEST_BUSINESS_ID);
+        verify(businessLookupRepository, times(0)).save(any(BusinessLookupEntity.class));
+
+        List<ILoggingEvent> loggedEvents = listAppender.list;
+        assertEquals(0, loggedEvents.size());
+    }
+
+    @Test
+    @DisplayName("Cannot update existing BusinessLookupEntity on invalid BusinessUpdatedEvent")
+    void testOn_whenInvalidBusinessUpdatedEventReceived_shouldThrowException() {
+        // Arrange
+        BusinessUpdatedEvent event = BusinessUpdatedEvent.builder()
+                .requestId(TEST_REQUEST_ID)
+                .businessId(TEST_BUSINESS_ID)
+                .businessName(TEST_BUSINESS_NAME)
+                .build();
+
+        String exceptionMessage = "Invalid business id";
+
+        when(businessLookupRepository.findByBusinessId(TEST_BUSINESS_ID)).thenReturn(new BusinessLookupEntity(TEST_BUSINESS_ID));
+
+        doAnswer(invocation -> {
+            BindingResult bindingResult = invocation.getArgument(1);
+            bindingResult.rejectValue("email", "error.invalid", exceptionMessage);
+            return null;
+        }).when(validator).validate(any(BusinessLookupEntity.class), any(BindingResult.class));
+
+        // Act & Assert
+        IllegalProjectionStateException exception = assertThrows(IllegalProjectionStateException.class, () -> {
+            businessLookupEventsHandler.on(event);
+        });
+
+        // Assert
+        assertEquals(exceptionMessage, exception.getLocalizedMessage());
+        verify(businessLookupRepository, times(1)).findByBusinessId(TEST_BUSINESS_ID);
+        verify(businessLookupRepository, times(0)).save(any(BusinessLookupEntity.class));
+
+        List<ILoggingEvent> loggedEvents = listAppender.list;
+        assertEquals(0, loggedEvents.size());
+    }
+
+    @Test
+    @DisplayName("Can delete existing BusinessLookupEntity on valid BusinessDeletedEvent")
+    void testOn_whenValidBusinessDeletedEventReceived_shouldDeleteBusinessLookupEntity() {
+        // Arrange
+        BusinessDeletedEvent event = BusinessDeletedEvent.builder()
+                .requestId(TEST_REQUEST_ID)
+                .businessId(TEST_BUSINESS_ID)
+                .build();
+
+        when(businessLookupRepository.findByBusinessId(TEST_BUSINESS_ID)).thenReturn(new BusinessLookupEntity(TEST_BUSINESS_ID));
+
+        // Act
+        businessLookupEventsHandler.on(event);
+
+        // Assert
+        verify(businessLookupRepository, times(1)).findByBusinessId(TEST_BUSINESS_ID);
+        verify(businessLookupRepository, times(1)).delete(any(BusinessLookupEntity.class));
+
+        List<ILoggingEvent> loggedEvents = listAppender.list;
+        assertEquals(1, loggedEvents.size());
+
+        loggingEvent = loggedEvents.get(0);
+        assertEquals(Level.INFO, loggingEvent.getLevel());
+        String expectedLogMessage = MessageFormatter.format(BUSINESS_DELETED_FROM_LOOKUP_DB, TEST_BUSINESS_ID).getMessage();
+        assertEquals(expectedLogMessage, loggingEvent.getFormattedMessage());
+
+        assertTrue(loggingEvent.getMarkerList().get(0).contains(Markers.append(REQUEST_ID, event.getRequestId())));
+        assertTrue(loggingEvent.getMarkerList().get(0).contains(Markers.append(BUSINESS_ID, event.getBusinessId())));
+    }
+
+    @Test
+    @DisplayName("Cannot delete BusinessLookupEntity for business that doesn't exist")
+    void testOn_whenBusinessDeletedEventReceivedForNonExistingBusiness_shouldThrowException() {
+        // Arrange
+        BusinessDeletedEvent event = BusinessDeletedEvent.builder()
+                .requestId(TEST_REQUEST_ID)
+                .businessId(TEST_BUSINESS_ID)
+                .build();
+
+        when(businessLookupRepository.findByBusinessId(TEST_BUSINESS_ID)).thenReturn(null);
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            businessLookupEventsHandler.on(event);
+        });
+
+        // Assert
+        assertEquals(String.format(BUSINESS_WITH_ID_DOES_NOT_EXIST, event.getBusinessId()), exception.getLocalizedMessage());
+        verify(businessLookupRepository, times(1)).findByBusinessId(TEST_BUSINESS_ID);
+        verify(businessLookupRepository, times(0)).delete(any(BusinessLookupEntity.class));
 
         List<ILoggingEvent> loggedEvents = listAppender.list;
         assertEquals(0, loggedEvents.size());
